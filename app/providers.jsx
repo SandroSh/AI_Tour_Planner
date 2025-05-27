6bb3028bede6e2396cd723547408393c92250162
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2025-05-27 13:23:10 +0400

    initialize query client

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,13 +1,27 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
-const Providers = ({children}) => {
+const Providers = ({ children }) => {
+    const [queryClient] = React.useState(() => {
+        return new QueryClient({
+            defaultOptions: {
+                // Refresh the data every 1 minute
+                queries: {
+                    staleTime: 1000 * 60 * 1, 
+                },
+            },
+        });
+    }
+    );
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster position="top-right" />
       {children}
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
